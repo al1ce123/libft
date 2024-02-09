7cb9862fa18c7d70eb1e8084d47ce5c71e9adf65
Nilo Lence Lefevre <nlence-l@k2r2p9.42mulhouse.fr>
2024-02-09 16:42:07 +0100

    adding some functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,52 +6,11 @@
 /*   By: nlence-l <nlence-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:20:52 by nlence-l          #+#    #+#             */
-/*   Updated: 2024/02/09 15:47:59 by nlence-l         ###   ########.fr       */
+/*   Updated: 2024/02/09 16:41:31 by nlence-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int get_lenght(const char *nptr)
-{
-    int i;
-    int len;
-
-    i = 0;
-    len = 0;
-    if (!nptr)
-        return (-1);
-    while (nptr[i] == ' ' || nptr[i] == '\t')
-        i++;
-    if (nptr[i] == '+') 
-        i++;
-    else if (nptr[i++] == '-')
-        i++;
-    if (nptr[i] && !ft_isdigit(nptr[i]))
-        return (-1);
-    else
-    {
-        while (nptr[i] && ft_isdigit(nptr[i]))
-        {
-            i++;
-            len++;
-        }
-        return (len);
-    }
-}
-
-static int get_pow(int len)
-{
-    int res;
-
-    res = 1;
-    while (len > 1)
-    {
-        res *= 10;
-        len--;
-    }
-    return (res);
-}
 
 int     ft_atoi(const char *nptr)
 {
@@ -61,14 +20,37 @@ int     ft_atoi(const char *nptr)
     int res;
 
     i = 0;
-    pow = get_pow(get_len(nptr));
-    if (pow < 0)
-        return (0);
+    pow = 1;
     sign = 1;
+    res = 0;
     if (!nptr)
         return (0);
-    while (nptr[i])
+    while (nptr[i] == ' ' || nptr[i] == '\t')
+        i++;
+    if (nptr[i] == '+') 
+        i++;
+    else if (nptr[i] == '-')
     {
-
+        sign *= -1;
+        i++;
     }
+    if (nptr[i] && !ft_isdigit(nptr[i]))
+        return (0);
+    else
+    {
+        while (nptr[i] && ft_isdigit(nptr[i]))
+            i++;
+        --i;
+        while (i >= 0 && ft_isdigit(nptr[i]))
+        {
+            res += nptr[i] - '0' * pow;
+            pow *= 10;
+        }
+    }
+    return (res * sign);
 }
+
+
+
+
+
--- test.c
+++ test.c
@@ -3,7 +3,8 @@
 
 int main(void) {
 
-    
+    printf("%d\n", ft_atoi("99"));
+    printf("%d\n", atoi("99"));
 
     return 0;
 }
